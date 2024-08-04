4ab318afe620f968682b2745f6c5179548975f94
RollsRoycewk <50765560@qq.com>
2024-08-04 18:17:02 +0800

    feat：IDEA配置tomcat

---
+++ web-all/web-01/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: ${NAME}
+ * Package:
+ * Description:
+ *
+ * @Author wk
+ * @Create 2024/8/4 09:12
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
