880c9290e6861fcedcf03b951da5b8706e5a878b
RollsRoycewk <50765560@qq.com>
2024-08-06 00:17:12 +0800

    feat：servletContext

--- web-all/web-03-servletConfig/src/com/atguigu/servlet/Servlet1.java
+++ web-all/web-03-servletConfig/src/com/atguigu/servlet/Servlet1.java
@@ -1,6 +1,7 @@
 package com.atguigu.servlet;
 
 import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,6 +22,7 @@ import java.util.Enumeration;
 public class Servlet1 extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("-------------------ServletConfig获取参数---------------");
         ServletConfig servletConfig = getServletConfig();
         // 获取初始配置信息即可
         // 根据参数名获取采参数值
@@ -34,6 +36,26 @@ public class Servlet1 extends HttpServlet {
         while (initParameterNames.hasMoreElements()) {
             String pname = initParameterNames.nextElement();
             System.out.println(pname + "=" + getInitParameter(pname));
+        }
+
+        System.out.println("-------------------ServletConfig获取参数---------------");
+        // 获取ServletContext
+        ServletContext servletContext1 = servletConfig.getServletContext();
+        ServletContext servletContext2 = req.getServletContext();
+        ServletContext servletContext3 = getServletContext();
+
+        System.out.println(servletContext1 == servletContext2);
+        //true
+        System.out.println(servletContext3 == servletContext2);
+        // true
+
+        String encoding = servletContext1.getInitParameter("encoding");
+        System.out.println("encoding:" + encoding);
+
+        Enumeration<String> parameterNames = servletContext1.getInitParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String pname = parameterNames.nextElement();
+            System.out.println(pname + "=" + servletContext1.getInitParameter(pname));
         }
     }
 }
